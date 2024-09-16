ec3c98a919e1f19c9fe3379ef4258a370bf31b07
RGSATG <136818066+RGSATG@users.noreply.github.com>
2024-09-16 18:13:49 +0200

    Clean up variables
    Remove clunky characterStyle variable. Change all references of
    "character" to "player".

--- logic.js
+++ logic.js
@@ -1,27 +1,26 @@
 "use strict";
 //This section is concerned with adding HTML elements
-const character = document.getElementById("character");
+const player = document.getElementById("player");
 const startButton = document.getElementById("startButton");
 
 //This section is concerned with starting the game
 startButton.addEventListener("click", () => {
-    character.style.display = "block";
-    character.style.left = addPx(playerX);
-    character.style.bottom = addPx(playerY);
+    player.style.display = "block";
+    player.style.left = addPx(playerX);
+    player.style.bottom = addPx(playerY);
     startButton.style.display = "none";
 }
 );
 
 //This section is concerned with the movement of the player
-const characterStyle = character.style;
 const upArrow = document.getElementById("upArrow");
 const downArrow = document.getElementById("downArrow");
 const rightArrow = document.getElementById("rightArrow");
 const leftArrow = document.getElementById("leftArrow");
-const playerWidth = character.style.width;
-const playerHeight = character.style.height;
-let playerX = characterStyle.left = getCenter(playerWidth, 0);
-let playerY = characterStyle.bottom = getCenter(playerHeight, 0);
+const playerWidth = player.style.width;
+const playerHeight = player.style.height;
+let playerX = player.style.left = getCenter(playerWidth, 0);
+let playerY = player.style.bottom = getCenter(playerHeight, 0);
 let stepLength = 30;
 
 function addPx(num) {
@@ -40,27 +39,27 @@ rightArrow.addEventListener("click", moveRight);
 function moveUp() {
     if (playerY < 570) {
         playerY += stepLength;
-        characterStyle.bottom = addPx(playerY);
+        player.style.bottom = addPx(playerY);
     }
 }
 
 function moveDown() {
     if (playerY > 0) {
         playerY -= stepLength;
-        characterStyle.bottom = addPx(playerY);
+        player.style.bottom = addPx(playerY);
     }
 }
 
 function moveRight() {
     if (playerX > 0) {
         playerX -= stepLength;
-        characterStyle.left = addPx(playerX);
+        player.style.left = addPx(playerX);
     }
 }
 
 function moveLeft() {
     if (playerX < 870) {
         playerX += stepLength;
-        characterStyle.left = addPx(playerX);
+        player.style.left = addPx(playerX);
     }
 }
