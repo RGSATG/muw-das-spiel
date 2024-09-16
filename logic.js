3830becc5dc61289c74cd56cada263df02a97605
RGSATG <136818066+RGSATG@users.noreply.github.com>
2024-09-16 18:08:01 +0200

    Change the way the player coordinates work
    The default player position is set inside Javascript so that the
    extractNumbers function is no longer needed. The coordinates Variable names are more intuitive. Coordinates now reference the center instead
    of the bottom left corner.

--- logic.js
+++ logic.js
@@ -1,52 +1,65 @@
+//This section is concerned with adding HTML elements
 const character = document.getElementById("character");
+const startButton = document.getElementById("startButton");
+
+//This section is concerned with starting the game
+startButton.addEventListener("click", () => {
+    character.style.display = "block";
+    character.style.left = addPx(playerX);
+    character.style.bottom = addPx(playerY);
+    startButton.style.display = "none";
+}
+);
+
+//This section is concerned with the movement of the player
 const characterStyle = character.style;
 const upArrow = document.getElementById("upArrow");
 const downArrow = document.getElementById("downArrow");
 const rightArrow = document.getElementById("rightArrow");
 const leftArrow = document.getElementById("leftArrow");
-let currentY = extractNumbers(window.getComputedStyle(character).getPropertyValue("bottom"));
-let currentX = extractNumbers(window.getComputedStyle(character).getPropertyValue("left"));
+const playerWidth = character.style.width;
+const playerHeight = character.style.height;
+let playerX = characterStyle.left = getCenter(playerWidth, 0);
+let playerY = characterStyle.bottom = getCenter(playerHeight, 0);
+let stepLength = 30;
+
+function addPx(num) {
+    return `${num}px`;
+}
+
+function getCenter(size, position) {
+    return position + size / 2;
+}
 
 upArrow.addEventListener("click", moveUp);
 downArrow.addEventListener("click", moveDown);
 leftArrow.addEventListener("click", moveLeft);
 rightArrow.addEventListener("click", moveRight);
 
-function extractNumbers(string) {
-    let numArr = [];
-    let splitString = string.split("");
-    for (let elem of splitString) {
-        if (elem.match("[0-9]")) {
-            numArr.push(elem);
-        }
-    }
-    return parseInt(numArr.join(""));
-}
-
 function moveUp() {
-    if (currentY < 570) {
-        currentY += 30;
-        characterStyle.bottom = `${currentY}px`;
+    if (playerY < 570) {
+        playerY += stepLength;
+        characterStyle.bottom = addPx(playerY);
     }
 }
 
 function moveDown() {
-    if (currentY > 0) {
-        currentY -= 30;
-        characterStyle.bottom = `${currentY}px`;
+    if (playerY > 0) {
+        playerY -= stepLength;
+        characterStyle.bottom = addPx(playerY);
     }
 }
 
 function moveRight() {
-    if (currentX > 0) {
-        currentX -= 30;
-        characterStyle.left = `${currentX}px`;
+    if (playerX > 0) {
+        playerX -= stepLength;
+        characterStyle.left = addPx(playerX);
     }
 }
 
 function moveLeft() {
-    if (currentX < 870) {
-        currentX += 30;
-        characterStyle.left = `${currentX}px`;
+    if (playerX < 870) {
+        playerX += stepLength;
+        characterStyle.left = addPx(playerX);
     }
 }
